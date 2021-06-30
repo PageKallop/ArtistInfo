45b43870f58badeb49d2916933522bdea6575d62
PageKallop <72150936+PageKallop@users.noreply.github.com>
2021-06-30 14:15:26 -0500

    Initial Commit

---
+++ ArtistInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtistInfo
+//
+//  Created by Page Kallop on 6/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
